6bfdd5c3342defc760af81d08e80a5e464330620
VilsonSouza <vilsonsouza758@gmail.com>
2022-10-14 10:58:17 -0300

    commit 14/10/22

---
+++ PacotesVisibilidade/untitled/src/Nomes.java
@@ -0,0 +1,14 @@
+
+public class Nomes {
+    //dado privado
+    private String nome;
+    // uso publico, para acessar em outra classe
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
